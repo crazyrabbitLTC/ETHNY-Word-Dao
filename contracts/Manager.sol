ab73b2369cd8259f1130a074f69fa2c2a0ca3ff3
Aseem Sood <asood123@yahoo.com>
2019-05-18 00:10:36 -0400

    working erc20 contract

---
+++ contracts/Manager.sol
@@ -0,0 +1,3 @@
+pragma solidity ^0.5.0;
+//import "openzeppelin-eth/contracts/token/StandaloneERC20.sol"
+
---
+++ contracts/WordDAOToken.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.0;
+
+import "openzeppelin-eth/contracts/token/ERC20/StandaloneERC20.sol";
+
+contract WordDAOToken is StandaloneERC20 {
+
+  function initialize(
+    string memory name, string memory symbol, uint8 decimals, uint256 initialSupply, address initialHolder,
+        address[] memory minters, address[] memory pausers
+    ) public initializer {
+      StandaloneERC20.initialize(name, symbol, decimals, initialSupply, initialHolder, minters, pausers);
+  }
+
+}
+
+// instance of contract: 0xA57B8a5584442B467b4689F1144D269d096A3daF
