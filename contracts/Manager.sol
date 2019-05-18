8dcceb0241343557679dbc06104855419120d706
Aseem Sood <asood123@yahoo.com>
2019-05-18 00:24:07 -0400

    interface definition of registry and manager

--- contracts/Manager.sol
+++ contracts/Manager.sol
@@ -1,3 +1,16 @@
 pragma solidity ^0.5.0;
 //import "openzeppelin-eth/contracts/token/StandaloneERC20.sol"
 
+contract Manager {
+  /*
+
+    addWord(word)
+      if word isn't there, adds it
+      issues token to
+
+    getWord(word) returns int
+      charges rent
+      returns words
+
+  */
+}
--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -2,4 +2,15 @@ pragma solidity ^0.5.0;
 
 contract Registry {
   
+  /*
+
+    map from int -> string
+
+    addWord(string word) returns integer
+      - only adds if word doesn't exist
+      - otherwise, error
+
+    getWord(int key) returns string or null
+
+  */
 }
